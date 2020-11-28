6604908cca84cf6235247dc17adf04cac7e1da9c
yuzesheng <1991615428@qq.com>
2020-11-28 16:05:53 +0800

    首次测试idea是否成功集成了git

---
+++ src/co/lianxi/gitStudy/demo01/HelloGit.java
@@ -0,0 +1,7 @@
+package co.lianxi.gitStudy.demo01;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello yuzesheng!welcome to git world!!");
+    }
+}
