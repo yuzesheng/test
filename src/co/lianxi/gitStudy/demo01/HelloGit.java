5e56258d5fa293cec9345740449ccc83e1c7b3e7
yuzesheng <1991615428@qq.com>
2020-11-28 16:16:18 +0800

    添加了两句6666啊

--- src/co/lianxi/gitStudy/demo01/HelloGit.java
+++ src/co/lianxi/gitStudy/demo01/HelloGit.java
@@ -1,7 +1,11 @@
 package co.lianxi.gitStudy.demo01;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 public class HelloGit {
     public static void main(String[] args) {
+        System.out.println("小余很6666啊");
+        System.out.println("那可不，肯定是6666啊");
         System.out.println("hello yuzesheng!welcome to git world!!");
     }
 }
